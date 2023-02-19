c01195e38f1af371587f99e9f14b4604ff637cec
rajupalumaru <raju.palumaru94@gmail.com>
2023-02-19 08:00:41 +0530

    Added left and right side components of Home Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <Login />
+          </Route>
+          <Route exact path='/home'>
+            <Header />
+            <Home/>
           </Route>
         </Switch>
       </Router>
